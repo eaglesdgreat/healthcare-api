bd6ba6e7b4b07894c25fc9b7f52309d750c2aeb4
Emmanuel <emmanuel.okponobi@zedcrest.com>
2026-03-30 23:35:21 +0100

    Database and hash token generation setup

---
+++ src/users/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ select: false }) // Exclude password from queries by default for security
+  password: string;
+
+  @Column({ default: 'PATIENT' })
+  role: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
