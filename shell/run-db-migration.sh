60834b5e4fcdc0c5bc0b9836f2bf1ec3bd4e280d
Emmanuel <emmanuel.okponobi@zedcrest.com>
2026-04-02 22:51:21 +0100

    Architecture setup and implementations

--- shell/run-db-migration.sh
+++ shell/run-db-migration.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 echo "Running database migrations..."
-# Using pnpm exec ensures it finds the binary in the container's node_modules
 pnpm exec db-migrate up -e $NODE_ENV
 echo "Database migration completed."
