380fce5004c8de3a39905b431e21975277300d37
Emmanuel <emmanuel.okponobi@zedcrest.com>
2026-04-11 07:13:41 +0100

    IMplemented user servcies anf controller

---
+++ src/common/interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+export interface PaginationResponse<T> {
+  data: T[]
+  meta: {
+    page: number
+    limit: number
+    total: number
+    totalPages: number
+    hasNextPage: boolean
+    hasPreviousPage: boolean
+  }
+}
---
+++ src/users/dto/index.ts
@@ -0,0 +1,5 @@
+import { PaginateUsersDto } from './paginate-user.dto'
+import { CreateUserDto } from './create-user.dto'
+import { SingleUserDTO } from './single-user.dto'
+
+export { PaginateUsersDto, CreateUserDto, SingleUserDTO }
