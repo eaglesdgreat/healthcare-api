bc9c84a1042405d8fa99e733d792575e30c750e0
Emmanuel <emmanuel.okponobi@zedcrest.com>
2026-04-19 22:35:01 +0100

    Added custom error exception handler filter to handle unknow errors in code

--- src/auth/auth.guards.ts
+++ src/auth/auth.guards.ts
@@ -2,34 +2,45 @@ import {
   CanActivate,
   ExecutionContext,
   Injectable,
-  // UnauthorizedException,
+  UnauthorizedException,
 } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
 import { Request } from 'express'
+import { User } from '@/users/entities/user.entity'
+import { IS_PUBLIC_KEY } from './auth.decorator'
+import { Reflector } from '@nestjs/core'
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(
     private jwtService: JwtService,
     private readonly configService: ConfigService,
+    private reflector: Reflector,
   ) {}
 
-  canActivate(context: ExecutionContext): boolean {
-    console.error(context)
-    // const request = context.switchToHttp().getRequest()
-    // const token = this.extractTokenFromHeader(request)
-    // if (!token) {
-    //   throw new UnauthorizedException()
-    // }
-    // try {
-    //   const payload = await this.jwtService.verifyAsync(token, {
-    //     secret: this.configService.get<string>('JWT_SECRET'),
-    //   })
-    //   request['user'] = payload
-    // } catch {
-    //   throw new UnauthorizedException()
-    // }
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+    if (isPublic) {
+      return true
+    }
+
+    const request: Request = context.switchToHttp().getRequest()
+    const token = this.extractTokenFromHeader(request)
+    if (!token) {
+      throw new UnauthorizedException()
+    }
+    try {
+      const payload: User = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>('JWT_SECRET'),
+      })
+      request['user'] = payload
+    } catch {
+      throw new UnauthorizedException()
+    }
     return true
   }
 
